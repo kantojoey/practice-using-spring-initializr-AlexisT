5714e2e4a44778c74098b21e4fd982cf0011d90c
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-01-19 18:46:27 -0600

    Created spring application using spring initializr

---
+++ demoalexis/src/main/java/com/examplealexis/demoalexis/DemoalexisApplication.java
@@ -0,0 +1,13 @@
+package com.examplealexis.demoalexis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoalexisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoalexisApplication.class, args);
+	}
+
+}
---
+++ demoalexis/src/test/java/com/examplealexis/demoalexis/DemoalexisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplealexis.demoalexis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoalexisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
